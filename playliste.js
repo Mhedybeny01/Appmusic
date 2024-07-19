f10fc41c81053df6974aec91639e72bbcb80213a
Mhedybeny01 <mehdibentaleb548@gmail.com>
2024-07-19 17:56:21 +0100

    Add files via upload

--- playliste.js
+++ playliste.js
@@ -1,39 +1,41 @@
-document.getElementById('login').onsubmit = function(e) {
-    let usernameInput = document.querySelector("[name='username']");
-    let passwordInput = document.querySelector("[name='password']");
-    let errorueser = document.getElementById('errorueser');
-    let errorpass = document.getElementById('errorpass');
+document.getElementById('login').addEventListener('submit',function(e){
+    e.preventDefault();
+   let test = false;
 
 
 
-    let usernameValid = usernameInput.value === "Mehdy";
-    let passwordValid = passwordInput.value === "12345678";
 
+    const erroruser = document.getElementById('errorueser');
+    const errorpassword = document.getElementById('errorpass');
+    erroruser.textContent="";
+    errorpassword.textContent="";
+    erroruser.style.display="non";
+    errorpassword.style.display="non";
 
-    if (!usernameValid || !passwordValid) {
-        e.preventDefault();
-    }
-
-
-    if(usernameInput !== usernameValid){
-        errorueser.textContent = "Username is incorrect....";
-        errorueser.style.display = "block";
-    }else{
-        errorueser.textContent = "Username is correct....";
-        
-    }
-
-    if(passwordInput !== passwordValid){
-        errorpass.textContent = "password is incorrect....";
-        errorpass.style.display = "block";
-    }else{
-        errorpass.textContent = "password is correct....";
-        
-        
-    }
 
     
-}
+    const username = document.getElementById('userin').value,password = document.getElementById('passin').value;
+
+   if(username === "mehdy"  &&  password === "2004"){
+       window.location.href='Artiste.html';
+       test = true;
+
+   }else if(username !== "mehdy"  &&  password === "2004"){
+        erroruser.textContent="Username is incorrect tryagain...";
+        erroruser.style.display="block";
+        
+        test = false;
+   }else{
+        errorpassword.textContent="password is incorrect tryagain...";
+        errorpassword.style.display="block";
+        test = false;
+        
+   }
+
+
+    
+
+});
 
 
 
